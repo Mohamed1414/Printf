62b54a17e0ce41b833ba47f65badf3421f58c2b4
Mohamed1414 <46685829+Mohamed1414@users.noreply.github.com>
2020-01-29 16:45:25 +0100

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbahstou <mbahstou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/22 16:50:45 by mbahstou          #+#    #+#             */
-/*   Updated: 2020/01/29 16:44:39 by mbahstou         ###   ########.fr       */
+/*   Created: 2020/01/29 15:21:25 by mbahstou          #+#    #+#             */
+/*   Updated: 2020/01/29 16:40:14 by mbahstou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include "ft_printf.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
 
-int		ft_printf(const char *format, ...)
-{
-	int			i;
-	t_printf	*pack;
-
-	i = 0;
-	if (!(pack = malloc(sizeof(t_printf))))
-		return (-1);
-	while (format[i] && format[i] != '%')
-		pack->size += write(1, &format[i++], 1);
-}
+typedef struct s_printf {
+	va_list	arg;
+	int		c;
+	int		size;
+} t_printf;
+#endif
